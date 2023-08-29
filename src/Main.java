38accc26e1378ed5a451efce4053cb9899052f97
unknown <baongantran2905@gmai.com>
2023-08-29 19:21:39 +0700

    demo demo

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int number = 1;
+        float numberFloat = 1.912897f;
+        double numberDouble = 1.912897d;
+        System.out.println(number);
+        System.out.println(numberFloat);
+        System.out.println(numberDouble);
+
+
+    }
+}
