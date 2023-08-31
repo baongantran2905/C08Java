0640f4ac57636e3bdd89b74b0fcf2a060888a60b
unknown <baongantran2905@gmai.com>
2023-08-31 14:30:17 +0700

    updated

---
+++ src/IFELSE.java
@@ -0,0 +1,17 @@
+public class IFELSE {
+    public static void main (String[] args){
+        int a = 12;
+        if (a + 2 == 10) {
+            System.out.println("true");
+        }
+        else if (a + 2 > 10) {
+            System.out.println("yesyesyes");
+        }
+        else if (a +2 <10){
+            System.out.println("hixhixhix");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
---
+++ src/TinhS.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class TinhS {
+    public static void main(String[] args) {
+        float a = 1.28f;
+        float b = 1.56f;
+        float S = a * b;
+        //Tinh S
+        System.out.println("Superficies=" + S);
+    }
+}
