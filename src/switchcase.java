c2528501685400a4519a014ddfb766b54d3dbd85
unknown <baongantran2905@gmai.com>
2023-08-31 14:46:13 +0700

    updated 2

--- src/switchcase.java
+++ src/switchcase.java
@@ -1,5 +1,22 @@
 public class switchcase {
     public static void main(String[] args){
-
+    int a = 27;
+    switch (a) {
+        case 12:
+            System.out.println("sai rui");
+            break;
+        case 21:
+            System.out.println("sai nua rui");
+            break;
+        case 87:
+            System.out.println("yeah yeah yeah");
+            break;
+        default:
+             System.out.println("lam lai di");
+             break;
+         }
+         }
     }
-}
+
+
+
